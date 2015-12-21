e46dddc2ab5dc4751a34dadb8a15d43323cf01d3
Matthias Uwe Berg-Neels <matthias.uwe.berg-neels@sap.com>
2015-12-21 12:24:56 +0100

    Vererbung Beispiele Teil 3

--- WWI15SEB/src/kapitel6/bauernhof/TestBauernhof.java
+++ WWI15SEB/src/kapitel6/bauernhof/TestBauernhof.java
@@ -3,6 +3,9 @@ package kapitel6.bauernhof;
 public class TestBauernhof {
 
 	public static void main(String[] args) {
+		
+		Tier[] stall = new Tier[5];
+		
 		/*
 		Tier irgendEinTier = new Tier(134.0f, "Weiss ich nicht", 15.0f );
 
@@ -13,6 +16,7 @@ public class TestBauernhof {
 		*/
 		
 		Hund bello = new Hund(15.6f, "Hund Bello", 50.7f, "Labrador");
+		stall[0] = bello;
 		
 		bello.bellen();
 		bello.atmen();
@@ -22,7 +26,7 @@ public class TestBauernhof {
 		//System.out.println("Unser Hund: " + bello.getBezeichnung() + ", " + bello.getGewicht() + ", " + bello.getGroesse());
 		
 		Fisch nemo = new Fisch(0.5f, "Fisch Nemo", 3.2f, false);
-		
+		stall[1] = nemo;
 		
 		nemo.blubbern();
 		nemo.atmen();
@@ -31,6 +35,25 @@ public class TestBauernhof {
 		System.out.println(nemo);
 		
 		//System.out.println("Unser Fisch: " + nemo.getBezeichnung() + ", " + nemo.getGewicht() + ", " + nemo.getGroesse());
+		
+		// Narrowing Cast
+		stall[2] = new Hund(35.4f, "Hund Wuff", 75.4f, "Schäferhund");
+		stall[3] = new Fisch(2.1f, "Fisch Dori", 6.5f, false);
+		stall[4] = new Fisch(1.1f, "Fisch Merlin", 4.5f, false);
+		
+		for(Tier einTier : stall){
+			einTier.fressen();
+			einTier.atmen();
+			System.out.println(einTier.toString());
+			
+			
+			if(einTier instanceof Hund){
+				// widening cast
+				Hund einHund = (Hund)einTier;
+				einHund.bellen();
+			}
+				
+		}
 	}
 
 }
